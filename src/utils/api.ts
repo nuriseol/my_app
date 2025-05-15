4aeb55c8269c84e275b302e89732936655e90b52
Nuri Seol <qqq4020@lottemart.com>
2025-05-15 16:30:48 +0900

    인터셉터 생성

---
+++ src/utils/api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+/**
+ * @name 공통API 호출 유틸
+ */
+const api = axios.create({
+  baseURL: process.env.REACT_APP_BASE_API,
+});
+
+// 요청 인터셉터
+api.interceptors.request.use(
+  async (config) => {
+    console.log("요청 보내기 전");
+    return config;
+  },
+  async (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+api.interceptors.response.use(
+  async (config) => {
+    console.log("응답 받은 후");
+    return config;
+  },
+  async (error) => {
+    return Promise.reject(error);
+  }
+);
