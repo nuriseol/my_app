9eb201fafe1701af789074a329374923b191b6b7
Nuri Seol <qqq4020@lottemart.com>
2025-05-15 10:55:57 +0900

    Tailwind CSS 지시문이 포함된 index.css를 index.tsx에 로드, tailwind 설정 기본 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // src 폴더 내의 모든 JS, JSX, TS, TSX 파일을 Tailwind CSS가 스캔하도록 설정
+  ],
   theme: {
-    extend: {},
+    extend: {
+      // 필요에 따라 테마 확장
+      // 예: colors: { primary: '#1DA1F2' },
+    },
   },
   plugins: [],
-}
-
+};
