9c67f09daec2008cf030c273a1df90f695150e7f
Nuri Seol <qqq4020@lottemart.com>
2025-08-18 14:41:28 +0900

    페이지네이션 css 수정

--- src/ClonePage/index.tsx
+++ src/ClonePage/index.tsx
@@ -1,18 +1,19 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
+import "./styles.css"; // 커스텀 CSS 파일을 import 합니다.
 import { Pagination, Mousewheel, Keyboard } from "swiper/modules";
 
 export default function ClonePage() {
   return (
     <Swiper
       direction="vertical"
-      slidesPerView={1}
-      spaceBetween={0}
+      slidesPerView={1} // 한 화면에 보이는 슬라이드 개수
+      spaceBetween={0} // 슬라이드 간의 간격
       mousewheel
       keyboard
-      pagination={{ clickable: true }}
-      modules={[Pagination, Mousewheel, Keyboard]}
+      pagination={{ clickable: true }} // clickable: true는 동그라미를 클릭해서 해당 슬라이드로 이동할 수 있게 합니다.
+      modules={[Pagination, Mousewheel, Keyboard]} // 필요한 모듈 설치
       className="h-screen"
       style={{ height: "100vh" }}
     >
